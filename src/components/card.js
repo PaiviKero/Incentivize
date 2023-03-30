4aca7a55c2bb84de1cbd8a1f8479a1e34780912e
Päivi Kero <paivi.kero@gmail.com>
2023-03-30 15:18:07 +0200

    styled-components in use

--- src/components/card.js
+++ src/components/card.js
@@ -1,42 +1,22 @@
-import { Text, StyleSheet, ImageBackground, View } from "react-native";
+import {
+  FlexView,
+  CardBackgroundImage,
+  CardTextView,
+  CardText,
+} from "./card.styles";
 
 export const Card = ({ quote = "test" }) => {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.coverImage}
+    <FlexView>
+      <CardBackgroundImage
         source={{
           uri: "https://loremflickr.com/320/240/brazil,rio",
         }}
       >
-        <View style={styles.textView}>
-          <Text style={styles.imageText}>{quote}</Text>
-        </View>
-      </ImageBackground>
-    </View>
+        <CardTextView>
+          <CardText>{quote}</CardText>
+        </CardTextView>
+      </CardBackgroundImage>
+    </FlexView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  coverImage: {
-    width: "100%",
-    height: "100%",
-  },
-  textView: {
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  imageText: {
-    fontSize: 20,
-    color: "white",
-    fontWeight: "bold",
-  },
-});
---
+++ src/components/card.styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components/native";
+import { ImageBackground, View, Text } from "react-native";
+
+export const FlexView = styled(View)`
+  flex: 1;
+`;
+
+export const CardBackgroundImage = styled(ImageBackground)`
+  width: 100%;
+  height: 100%;
+`;
+
+export const CardTextView = styled(View)`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+export const CardText = styled(Text)`
+  font-size: 20;
+  color: white;
+  font-weight: bold;
+`;
